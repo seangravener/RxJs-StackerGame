a1ae77dca568fe57b2bc70d56cda78d9e5a1b5b4
Sean Gravener <sean@gravener.net>
2022-12-31 06:33:16 -0500

    Stub initial RxJS streams

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,26 @@ import {
   ViewEncapsulation,
 } from '@angular/core';
 import { generateGrid, NUM_OF_COLUMNS } from './generateGrid';
+import { combineLatest, fromEvent, interval, map, scan } from 'rxjs';
 
 type GridCoords = [number, number];
 
 const state = {
   gameOver: false,
   cells: [...generateGrid()],
+};
+
+// use the scan operator to keep track of the current level and the speed at which the blocks are moving
+const initialState = {
+  level: 1,
+  speed: 1000,
+  previousBlockPos: { x: 0, y: 0 },
+  gameOver: false,
+  // add default values for other properties
+  blocks: [],
+  score: 0,
+  highScore: 0,
+  update: {},
 };
 
 @Component({
@@ -23,31 +37,37 @@ const state = {
 export class AppComponent implements AfterViewInit {
   @ViewChild('container') container: ElementRef;
 
-  activePosition = [[6, 2], 3];
+  blockMovement$ = interval(1000).pipe(
+    map((val) => {
+      // calculate the position of the blocks based on the value emitted by the interval stream
+      return {
+        x: val % 3, // block can be in 3 positions (0, 1, 2)
+        y: Math.floor(val / 3), // block can be in 3 rows (0, 1, 2)
+      };
+    })
+  );
+
+  button = document.querySelectorAll('body');
+  buttonClicks$ = fromEvent(this.button, 'click');
+
+  game$ = combineLatest([this.blockMovement$, this.buttonClicks$]);
+
+  gameState$ = this.game$.pipe(
+    scan((state, [update, click]) => {
+      console.log(state);
+      console.log(update);
+
+      return { ...state, previousBlockPos: update };
+    }, initialState)
+  );
 
   get grid() {
     return [...state.cells];
   }
 
   ngAfterViewInit() {
-    console.log(this.container.nativeElement);
-    console.log(state);
-
-    this.setPosition([4, 5], 3, true);
-  }
-
-  setPosition([row, col]: GridCoords, tailLen: number, isActive: boolean) {
-    console.log('all cells', state.cells);
-    // const value = isActive ? 1 : 0;
-    const value = tailLen;
-    const position = row * NUM_OF_COLUMNS + col;
-
-    // state.cells[position - 1] = value;
-    const head = [value];
-    const tail = state.cells.slice(position, position + tailLen).fill(value);
-
-    console.log('tail', tail);
-    console.log('positions', '=>', position - 1, position);
-    console.log('value', head);
+    this.gameState$.subscribe((state) => {
+      // console.log('state in sub: ', state);
+    });
   }
 }
